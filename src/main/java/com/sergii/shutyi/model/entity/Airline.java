45012e74059537c60fa6a487db5c497c40478279
quar17esma <quar17esma@gmail.com>
2017-08-14 16:45:35 +0300

    added methods and fields

--- src/main/java/com/sergii/shutyi/model/entity/Airline.java
+++ src/main/java/com/sergii/shutyi/model/entity/Airline.java
@@ -11,6 +11,33 @@ public class Airline {
      */
     private ArrayList<Aircraft> aircraftList = new ArrayList<>();
 
+    /**
+     * Passenger capacity of the Airline
+     */
+    private int passengerCapacity;
+
+    /**
+     * Carrying capacity of the Airline
+     */
+    private int carryingCapacity;
+
+    /**
+     * Method sorts list of aircraft by flight range in ascend order
+     * @param aircraftList list of aircraft to sort
+     */
+    public void sortByFlightRange(ArrayList<Aircraft> aircraftList){
+    }
+
+    /**
+     * Method finds Aircraft with fuel consumption in range.
+     * @param minFuelConsumption minimal fuel consumption
+     * @param maxFuelConsumption maximal fuel consumption
+     * @return list of aircraft with fuel consumption in range
+     */
+    public ArrayList<Aircraft> findByFuelConsumptionRange(int minFuelConsumption, int maxFuelConsumption){
+        return null;
+    }
+
     //getter and setter
     public ArrayList<Aircraft> getAircraftList() {
         return aircraftList;
@@ -18,5 +45,13 @@ public class Airline {
 
     public void setAircraftList(ArrayList<Aircraft> aircraftList) {
         this.aircraftList = aircraftList;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public int getCarryingCapacity() {
+        return carryingCapacity;
     }
 }
