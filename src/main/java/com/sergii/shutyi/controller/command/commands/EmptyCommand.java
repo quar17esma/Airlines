fdfa74f998e046081d68ef03d6539d06ec194396
quar17esma <quar17esma@gmail.com>
2017-08-14 16:04:03 +0300

    change implamentation

--- src/main/java/com/sergii/shutyi/controller/command/commands/EmptyCommand.java
+++ src/main/java/com/sergii/shutyi/controller/command/commands/EmptyCommand.java
@@ -8,7 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 public class EmptyCommand implements ActionCommand {
     @Override
     public String execute(HttpServletRequest request) {
-        String page = ConfigurationManager.getProperty("path.page.add.note");
+        String page = null;
+//        page = ConfigurationManager.getProperty("path.page.add.note");
         return  page;
     }
 }
