d7c84651d152dee21b46421a572cbd14432eae36
quar17esma <quar17esma@gmail.com>
2017-08-14 15:47:13 +0300

    Added Commands

---
+++ src/main/java/com/sergii/shutyi/controller/command/CommandEnum.java
@@ -0,0 +1,30 @@
+package com.sergii.shutyi.controller.command;
+
+import com.sergii.shutyi.controller.command.commands.ShowNotebookCommand;
+
+/**
+ * Possible Commands.
+ */
+public enum CommandEnum {
+    ADD_NOTE{
+        {
+            this.command = new AddNoteCommand();
+        }
+    },
+    SHOW_NOTEBOOK{
+        {
+            this.command = new ShowNotebookCommand();
+        }
+    },
+    INPUT_NOTE{
+        {
+            this.command = new InputNoteCommand();
+        }
+    };
+
+    ActionCommand command;
+
+    public ActionCommand getCurrentCommand(){
+        return command;
+    }
+}
