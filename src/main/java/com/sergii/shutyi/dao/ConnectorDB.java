dd5a2f2d77923d517cde7f315e382e7dc62526ef
quar17esma <quar17esma@gmail.com>
2017-08-14 21:56:45 +0300

    added DBConnector

---
+++ src/main/java/com/sergii/shutyi/dao/ConnectorDB.java
@@ -0,0 +1,18 @@
+package com.sergii.shutyi.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ConnectorDB {
+
+    public static Connection getConnection() throws SQLException {
+        ResourceBundle resource = ResourceBundle.getBundle("database");
+        String url = resource.getString("db.url");
+        String user = resource.getString("db.user");
+        String password = resource.getString("db.password");
+
+        return DriverManager.getConnection(url, user, password);
+    }
+}
