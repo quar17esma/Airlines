c397b32ab4efb592d414f88be35e1fdc942b1b3f
quar17esma <quar17esma@gmail.com>
2017-08-14 16:04:54 +0300

    optimized imports

--- src/main/java/com/sergii/shutyi/controller/command/commands/EmptyCommand.java
+++ src/main/java/com/sergii/shutyi/controller/command/commands/EmptyCommand.java
@@ -1,8 +1,6 @@
 package com.sergii.shutyi.controller.command.commands;
 
 import com.sergii.shutyi.controller.command.ActionCommand;
-import com.sergii.shutyi.controller.manager.ConfigurationManager;
-
 import javax.servlet.http.HttpServletRequest;
 
 public class EmptyCommand implements ActionCommand {
