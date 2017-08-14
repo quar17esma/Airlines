368b99336bedffb9da6b40eda7643c97d2225fec
quar17esma <quar17esma@gmail.com>
2017-08-15 02:46:35 +0300

    use ConnectorDB

--- src/main/java/com/sergii/shutyi/dao/ConnectorDB.java
+++ src/main/java/com/sergii/shutyi/dao/ConnectorDB.java
@@ -3,16 +3,34 @@ package com.sergii.shutyi.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
-public class ConnectorDB {
+public class ConnectorDB implements AutoCloseable{
+    private static Connection connection;
 
     public static Connection getConnection() throws SQLException {
         ResourceBundle resource = ResourceBundle.getBundle("database");
         String url = resource.getString("db.url");
-        String user = resource.getString("db.user");
-        String password = resource.getString("db.password");
 
-        return DriverManager.getConnection(url, user, password);
+        Properties properties = new Properties();
+        properties.put("user", resource.getString("db.user"));
+        properties.put("password", resource.getString("db.password"));
+        properties.put("autoReconnect", resource.getString("db.autoReconnect"));
+        properties.put("characterEncoding", resource.getString("db.characterEncoding"));
+        properties.put("useUnicode", resource.getString("db.useUnicode"));
+        properties.put("useJDBCCompliantTimezoneShift", resource.getString("db.useJDBCCompliantTimezoneShift"));
+        properties.put("useLegacyDatetimeCode", resource.getString("db.useLegacyDatetimeCode"));
+        properties.put("serverTimezone", resource.getString("db.serverTimezone"));
+
+        DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+        connection = DriverManager.getConnection(url, properties);
+        return connection;
+    }
+
+    @Override
+    public void close() throws Exception {
+        connection.close();
     }
 }
--- src/main/java/com/sergii/shutyi/dao/PlaneDAO.java
+++ src/main/java/com/sergii/shutyi/dao/PlaneDAO.java
@@ -3,42 +3,18 @@ package com.sergii.shutyi.dao;
 import com.sergii.shutyi.model.entity.plane.AbstractPlane;
 import com.sergii.shutyi.model.entity.plane.CargoPlane;
 import com.sergii.shutyi.model.entity.plane.PassengerPlane;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-import java.util.ResourceBundle;
 
 public class PlaneDAO {
-    ResourceBundle resource = ResourceBundle.getBundle("database");
-    String url = resource.getString("db.url");
-    String user = resource.getString("db.user");
-    String password = resource.getString("db.password");
 
     public static final String SQL_SELECT_ALL_PLANES = "SELECT * FROM planes";
 
     public List<AbstractPlane> findAll() {
         List<AbstractPlane> planes = new ArrayList<>();
 
-        ResourceBundle resource = ResourceBundle.getBundle("database");
-        Properties properties = new Properties();
-        properties.put("user", resource.getString("db.user"));
-        properties.put("password", resource.getString("db.password"));
-        properties.put("autoReconnect", resource.getString("db.autoReconnect"));
-        properties.put("characterEncoding", resource.getString("db.characterEncoding"));
-        properties.put("useUnicode", resource.getString("db.useUnicode"));
-        properties.put("useJDBCCompliantTimezoneShift", resource.getString("db.useJDBCCompliantTimezoneShift"));
-        properties.put("useLegacyDatetimeCode", resource.getString("db.useLegacyDatetimeCode"));
-        properties.put("serverTimezone", resource.getString("db.serverTimezone"));
-
-        try {
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        try (Connection connection = DriverManager.getConnection(url, properties);
+        try (Connection connection = ConnectorDB.getConnection();
              Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(SQL_SELECT_ALL_PLANES);
             while (resultSet.next()) {
