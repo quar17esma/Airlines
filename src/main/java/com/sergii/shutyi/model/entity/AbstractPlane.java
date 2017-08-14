7e8fb82f7c56b6bfebcde5a692c59ccdd5f20ee8
quar17esma <quar17esma@gmail.com>
2017-08-14 18:09:55 +0300

    edited comments

--- src/main/java/com/sergii/shutyi/model/entity/AbstractPlane.java
+++ src/main/java/com/sergii/shutyi/model/entity/AbstractPlane.java
@@ -17,17 +17,17 @@ public class AbstractPlane implements Aircraft {
     private int passengerCapacity;
 
     /**
-     * Carrying capacity of the plane
+     * Carrying capacity of the plane in kg.
      */
     private int carryingCapacity;
 
     /**
-     * Flight range of the plane. How far it can flight
+     * Flight range of the plane. How far it can flight in km.
      */
     private int flightRange;
 
     /**
-     * Fuel consumption of the plane.
+     * Fuel consumption of the plane. Litres on 100 kilometers
      */
     private int fuelConsumption;
 
