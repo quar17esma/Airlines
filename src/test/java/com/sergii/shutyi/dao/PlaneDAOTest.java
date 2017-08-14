cf8db2499cc6b88b015f277a65da380528ebbb69
quar17esma <quar17esma@gmail.com>
2017-08-15 02:25:54 +0300

    Added Test

--- src/test/java/com/sergii/shutyi/dao/PlaneDAOTest.java
+++ src/test/java/com/sergii/shutyi/dao/PlaneDAOTest.java
@@ -1,5 +1,19 @@
+package com.sergii.shutyi.dao;
+
+import com.sergii.shutyi.model.entity.AbstractPlane;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PlaneDAOTest {
+    @Test
+    public void findAll() throws Exception {
+        PlaneDAO planeDAO = new PlaneDAO();
+        List<AbstractPlane> planes = planeDAO.findAll();
+        System.out.println(planes);
+    }
 
 }
