fb04d522708c6e37f3ff8bbc31a1e670712abe10
quar17esma <quar17esma@gmail.com>
2017-08-16 22:44:19 +0300

    added new fields, getters and setters

--- src/main/java/com/sergii/shutyi/model/entity/Airline.java
+++ src/main/java/com/sergii/shutyi/model/entity/Airline.java
@@ -1,8 +1,10 @@
 package com.sergii.shutyi.model.entity;
 
+import com.sergii.shutyi.dao.AirlineDAO;
 import com.sergii.shutyi.dao.PlaneDAO;
 import com.sergii.shutyi.model.util.SortPlane;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,7 +19,15 @@ public class Airline {
      */
     private List<Aircraft> aircraftList;
 
-//    private String name;
+    private String name;
+
+    private String headquarters;
+
+    private String hubs;
+
+    private String parentCompany;
+
+    private LocalDate founded;
 
     /**
      * Passenger capacity of the Airline
@@ -34,8 +44,8 @@ public class Airline {
         this.aircraftList = planeDAO.findAll();
         this.passengerCapacity = calculatePassengerCapacity(this.aircraftList);
         this.carryingCapacity = calculateCarryingCapacity(this.aircraftList);
-//        AirlineDAO airlineDAO = new AirlineDAO();
-//        this.name = airlineDAO.name;
+        AirlineDAO airlineDAO = new AirlineDAO();
+        airlineDAO.setAirlineParameters(this);
     }
 
     public int calculatePassengerCapacity(List<Aircraft> aircraftList) {
@@ -121,15 +131,47 @@ public class Airline {
         return carryingCapacity;
     }
 
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        if (!Objects.requireNonNull(name).isEmpty()) {
-//            this.name = name;
-//        } else {
-//            throw new IllegalArgumentException("airline name can't be empty");
-//        }
-//    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (!Objects.requireNonNull(name).isEmpty()) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("airline name can't be empty");
+        }
+    }
+
+    public String getHeadquarters() {
+        return headquarters;
+    }
+
+    public void setHeadquarters(String headquarters) {
+        this.headquarters = headquarters;
+    }
+
+    public String getHubs() {
+        return hubs;
+    }
+
+    public void setHubs(String hubs) {
+        this.hubs = hubs;
+    }
+
+    public String getParentCompany() {
+        return parentCompany;
+    }
+
+    public void setParentCompany(String parentCompany) {
+        this.parentCompany = parentCompany;
+    }
+
+    public LocalDate getFounded() {
+        return founded;
+    }
+
+    public void setFounded(LocalDate founded) {
+        this.founded = founded;
+    }
 }
