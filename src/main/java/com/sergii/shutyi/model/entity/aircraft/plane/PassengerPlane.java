1ce3f9cd11d299f46b3c57149e3fef8052913a24
quar17esma <quar17esma@gmail.com>
2017-08-18 01:02:49 +0300

    added specific fields

--- src/main/java/com/sergii/shutyi/model/entity/aircraft/plane/PassengerPlane.java
+++ src/main/java/com/sergii/shutyi/model/entity/aircraft/plane/PassengerPlane.java
@@ -3,6 +3,11 @@ package com.sergii.shutyi.model.entity.aircraft.plane;
 public class PassengerPlane extends AbstractPlane {
     private static final int CARRYING_CAPACITY = 0;
 
+    /**
+     * Volume of passenger compartment in cubic meters
+     */
+    private int passengerCompartmentVolume;
+
     @Override
     public int getCarryingCapacity() {
         return CARRYING_CAPACITY;
@@ -11,5 +16,13 @@ public class PassengerPlane extends AbstractPlane {
     @Override
     public void setCarryingCapacity(int carryingCapacity) {
         throw new UnsupportedOperationException();
+    }
+
+    public int getPassengerCompartmentVolume() {
+        return passengerCompartmentVolume;
+    }
+
+    public void setPassengerCompartmentVolume(int passengerCompartmentVolume) {
+        this.passengerCompartmentVolume = passengerCompartmentVolume;
     }
 }
